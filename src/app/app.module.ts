53d2b077d0445365a5bcab783b636aff77172cbc
Jac Bodewes <jhg_bodewes@hotmail.com>
2018-05-28 21:32:20 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import {AppComponent} from "./app.component";
+import { MyComponentComponent } from './my-component/my-component.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  imports: [BrowserModule],
+  declarations: [AppComponent, MyComponentComponent],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+
+}
